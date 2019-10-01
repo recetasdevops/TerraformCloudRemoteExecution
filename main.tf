9cdc5fd4e95456811c6b271f8df59d38c30d16ea
recetasdevops <44634828+recetasdevops@users.noreply.github.com>
2019-10-01 22:21:14 +0200

    First upload

---
+++ main.tf
@@ -0,0 +1,36 @@
+provider "azurerm" {
+  version = "=1.34.0"
+}
+
+resource "azurerm_resource_group" "TerraformCloudRG" {
+  name     = "TerraformCloud"
+  location = var.location
+
+  tags = {
+    terraform = "Terraform Cloud"
+  }
+}
+
+resource "azurerm_sql_server" "sql_server" {
+  name                         = var.sql_server_database_name
+  resource_group_name          = azurerm_resource_group.TerraformCloudRG
+  location                     = var.location
+  version                      = "12.0"
+  administrator_login          =  var.sql_server_user
+  administrator_login_password =  var.sql_server_password
+  tags = {
+    terraform = "Terraform Cloud"
+  }
+}
+
+
+resource "azurerm_sql_database" "sql_database" {
+  name                = azurerm_sql_server.sql_server.name
+  resource_group_name = azurerm_resource_group.TerraformCloudRG
+  location            = var.location
+  server_name         = azurerm_sql_server.sql_server.name
+
+  tags = {
+    terraform = "Terraform Cloud"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "location" {
+  default     = "westeurope"
+  description = "Resource location"
+}
+
+variable "sql_server_name" {
+  description = "Sql Server name"
+}
+
+variable "sql_server_database_name" {
+  description = "Sql Server database name"
+}
+
+variable "sql_server_user" {
+  description = "Sql Server user"
+}
+variable "sql_server_password" {
+  description = "Sql Server password"
+}
